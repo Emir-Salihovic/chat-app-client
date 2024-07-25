1cb845f59e24f4dd78fbf64e2329a548ea0a0fd6
Emir Salihovic <emir.home.11@gmail.com>
2024-07-25 22:38:41 +0200

    chore(config): restored production env

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
-  // baseURL: import.meta.env.VITE_PROD_SERVER,
+  // baseURL: "http://localhost:8000/api/v1",
+  baseURL: import.meta.env.VITE_PROD_SERVER,
   withCredentials: true,
 });
